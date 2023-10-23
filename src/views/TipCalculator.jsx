8d8bcfa2cb9bdcf6c40bdbab456b79bbaafa6db1
Harshana Wijesinghe <harshawije99@gmail.com>
2023-10-24 01:24:09 +0530

    refactoring

--- src/views/TipCalculator.jsx
+++ src/views/TipCalculator.jsx
@@ -1,23 +1,14 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { increment, reset } from "../features/slices/calculateSlice";
-import elementChecker from "../utils/arrElementChecker";
 
 const TipCalculator = () => {
   const [subTotal, setSubtotal] = useState(0);
-  const [tip, setTip] = useState([]);
 
   const amount = useSelector((state) => state.calc.value);
   const dispatch = useDispatch();
 
   useMemo(() => {
-    setTip([...tip, amount]);
-
-    // if (amount === 0) {
-    //   const sum = elementChecker(subTotal, tip.at(-1));
-    //   setSubtotal(() => sum);
-    // }
-
     setSubtotal(amount + subTotal);
   }, [amount]);
 
